ad4ede51aa050cec3e64f05f61af779188444b75
MykhailoMamin <mishamamin2016@gmail.com>
2023-10-30 19:37:52 +0200

    first commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -41,6 +41,18 @@ export default class App extends Component {
     }));
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    this.setState({ contacts: parsedContacts });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const normalisedFilter = this.state.filter.toLowerCase();
 
